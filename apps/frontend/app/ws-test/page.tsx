f1859ec52e15221664fc85a9868659f13682c6fb
Amrutanshu <amrutanshumishra607@gmail.com>
2026-01-19 18:44:10 +0530

    step5: websocket echo broadcast between clients

--- apps/frontend/app/ws-test/page.tsx
+++ apps/frontend/app/ws-test/page.tsx
@@ -4,7 +4,7 @@
 import React, { useEffect, useState } from "react";
 
 const WebSocketClient = () => {
-     const [messages, setMessages] = useState([]);
+     const [messages, setMessages] = useState<string[]>([]);
      const [input, setInput] = useState('');
      const [ws, setWs] = useState<WebSocket | null>(null);
 
@@ -14,6 +14,10 @@ const WebSocketClient = () => {
           socket.onopen = () => {
                console.log('Connected to websocket server');
                setWs(socket);
+          };
+
+          socket.onmessage = async (data) => {
+               console.log(data);
           };
 
           socket.onclose = () => {
@@ -28,9 +32,37 @@ const WebSocketClient = () => {
           }
      }, []);
 
-     return(
+     const sendMessage = () => {
+          if (ws && ws.readyState === WebSocket.OPEN) {
+               ws.send(input);
+               setMessages((prevMessages) => [...prevMessages, `Sent: ${input}`]);
+               setInput('');
+          }
+          else{
+               console.log('WebSocket is not connected');
+          }
+     }
+
+     return (
           <div>
                Web socket server testing
+               <input
+                    type="text"
+                    value={input}
+                    onChange={(e)=> setInput(e.target.value)}
+                    placeholder="Send a message"
+               />
+               <button onClick={sendMessage}>
+                    Click Me
+               </button>
+               <div>
+                    <h2>Messages:</h2>
+                    <ul>
+                         {messages.map((msg,index) => (
+                              <li key={index}>{msg}</li>
+                         ))}
+                    </ul>
+               </div>
           </div>
      )
 };
--- services/ws-gateway/server.js
+++ services/ws-gateway/server.js
@@ -2,12 +2,21 @@ import WebSocket, {WebSocketServer} from "ws";
 
 const wss = new WebSocketServer({port: 8080});
 
+//Data structure to hold all connected clients
+
 wss.on('connection', function connection(ws) {
+  //add clients whenever they are added
+
   ws.send('client connected');
   console.log("client has been connected");
 
-  ws.on('message', function message(data) {
+  ws.on('message', function message(data, isBinary) {
     console.log('received: %s', data);
+    wss.clients.forEach((client) =>{
+      if(client.readyState === WebSocket.OPEN){
+        client.send(data, {binary:isBinary});
+      }
+    });
   });
 
   ws.on('close', function disconnection(){
